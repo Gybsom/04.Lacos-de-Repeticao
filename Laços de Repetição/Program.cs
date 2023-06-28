2697e6a0f6d4ce50aa283d31fa009d92b7edd68d
Gybsom Souza <gybsom.souza@gmail.com>
2023-06-28 13:34:59 -0300

    publish

---
+++ Laços de Repetição/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int index1 = 1, index2 = 1;
+
+        while (index1 <= 10)
+        {
+            Console.WriteLine("Execução While nº " + index1);
+            index1++;
+        }
+
+        Console.WriteLine("--------------");
+
+        do 
+        {
+            Console.WriteLine("Execução Do While nº " + index2);
+            index2++;
+        } 
+        while (index2 <= 10);
+
+        Console.WriteLine("--------------");
+
+        for (int i = 10; i < 0; i--)
+        {
+            Console.WriteLine("Execução For nº " + (i+1));
+        }
+
+        Console.WriteLine("--------------");
+
+        int[] arrayDeInteiros = {3, 5, 7, 9};
+
+        foreach (int i in arrayDeInteiros)
+        {
+            Console.WriteLine("Valor atual: " + i);
+        }
+
+    }
+}
